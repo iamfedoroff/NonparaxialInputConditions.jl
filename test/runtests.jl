9c05c94c498dee44622d84b5c805a6133a662ed4
iamfedoroff <iamfedoroff@gmail.com>
2020-12-15 12:56:58 +0300

    Fix tests. Ranges are not accepted anymore as the argument for nlsolve.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ function zs_func(r)
     return r^2 / (4 * f)   # parabola
 end
 
-R = range(1e-3, 1.0, length=100)
+R = Array(range(1e-3, 1.0, length=100))
 
 rth = @. 2 * f^2 / R * (sqrt(1 + R^2 / f^2) - 1)
 Yth = @. 1 - rth^2 / (4 * f^2)
